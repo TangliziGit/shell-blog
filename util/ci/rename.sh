a924c3375f1a34383ac369ac3e06ce88a4f5ba00
TangliziGit <tanglizimail@foxmail.com>
2020-01-19 22:32:19 +0800

    modify ci script rename.sh

--- util/ci/rename.sh
+++ util/ci/rename.sh
@@ -2,7 +2,7 @@ code=0
 date="$(date +%Y-%m-%d_)"
 for filename in `find post -name '*.md' -printf '%P\n'`; do
     prefix="$(echo "$filename" | egrep "[0-9]{4}-[0-9]{2}-[0-9]{2}_")"
-    if [[ $prefix == "" ]]; then
+    if [ X$prefix = X"" ]; then
         echo "\`$filename\` is not in correct time format. (do you means $date$filename?)"
         mv "post/$filename" "post/$date$filename"
         code=1
