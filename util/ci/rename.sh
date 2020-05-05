1a52cd49a91bfca03670f8363bf8227d68683891
TangliziGit <tanglizimail@foxmail.com>
2020-05-05 16:42:53 +0800

    add new function, redirectly visit html pages

--- util/ci/rename.sh
+++ util/ci/rename.sh
@@ -1,11 +1,17 @@
 code=0
 date="$(date +%Y-%m-%d_)"
-for filename in `find post -name '*.md' -printf '%P\n'`; do
-    prefix="$(echo "$filename" | egrep "[0-9]{4}-[0-9]{2}-[0-9]{2}_")"
-    if [ X$prefix = X"" ]; then
-        echo "\`$filename\` is not in correct time format. (do you means $date$filename?)"
-        mv "post/$filename" "post/$date$filename"
-        code=1
-    fi
-done
+function rename(){
+    for filename in `find $1 -name "*.$2" -printf '%P\n'`; do
+        prefix="$(echo "$filename" | egrep "[0-9]{4}-[0-9]{2}-[0-9]{2}_")"
+        if [ X$prefix = X"" ]; then
+            echo "\`$filename\` is not in correct time format. (do you means $date$filename?)"
+            mv "$1/$filename" "$1/$date$filename"
+            code=1
+        fi
+    done
+}
+
+rename "post" "md"
+rename "page" "html"
+
 exit $code
